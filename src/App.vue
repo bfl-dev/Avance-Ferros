3c27637f88692cd4b755db4cd91f2f1d876c3efa
bfl-dev <paparrasilva2@gmail.com>
2024-10-18 00:46:36 -0300

    feat: added router to the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup>
-
 </script>
 
 <template>
   <p>Placeholder</p>
+  <router-view></router-view>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,5 +2,25 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import Logo from '@/components/icons/Logo.vue'
+import Home from '@/pages/Home.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    path: '/',
+    component: Home
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes
+})
+
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
