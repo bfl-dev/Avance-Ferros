425e15fe53b857a6b11ca264a0bee118acf44528
bfl-dev <paparrasilva2@gmail.com>
2024-10-19 17:26:02 -0300

    feat: added functional login

---
+++ src/api/UserApi.js
@@ -0,0 +1,21 @@
+﻿import axios from 'axios';
+
+export default class UserApi {
+
+
+
+  static getUser(userId) {
+    return axios.get('http://localhost:3000/userHead/' + userId);
+  }
+
+  static postUser(user, email, pass) {
+    const data = {
+      user: user,
+      email: email,
+      pass: pass
+    };
+    return axios.post('http://localhost:3000/userHead', data);
+  }
+}
+
+
