fa564cfd8d8f3845946e5351632403ddfb286319
Strawberry Frappé <65193599+StrawberryFrappe@users.noreply.github.com>
2024-10-22 00:39:07 -0300

    Update TimeUtils.js

--- src/api/TimeUtils.js
+++ src/api/TimeUtils.js
@@ -5,3 +5,10 @@ export function convertToTime(number){
 export function convertToMinutes(text){
   return parseInt(text.toString().split(':')[0])*60 + parseInt(text.toString().split(':')[1])
 }
+export function getCurrentDate() {
+  const today = new Date();
+  const yyyy = today.getFullYear();
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+  const dd = String(today.getDate()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd}`;
+}
