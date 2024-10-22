3f06fd48504ac60363e49928cb4683a140a65df7
Strawberry Frappé <j.saez18@ufromail.cl>
2024-10-22 16:54:40 -0300

    Update SeatComponent.vue

--- src/components/TravelInfo/SeatComponent.vue
+++ src/components/TravelInfo/SeatComponent.vue
@@ -13,23 +13,63 @@ const localSeat = ref(props.seat.status)
 </script>
 
 <template>
-  <div class="seat" v-if="localSeat==='0'" @click="update">
-    <img class="seat" src="../../assets/Libre.png" v-show="!selected">
-    <img class="seat" src="../../assets/Seleccionado.png" v-show="selected">
+  <div class="seat-wrapper">
+    <div class="seat-container">
+      <p class="number">{{seat.id}}</p>
+      <div class="seat" v-if="localSeat==='0'" @click="update">
+        <img class="seat-image" src="../../assets/Libre.png" v-show="!selected">
+        <img class="seat-image" src="../../assets/Seleccionado.png" v-show="selected">
+      </div>
+      <div class="seat" v-if="localSeat==='1'">
+        <img class="seat-image" src="../../assets/Ocupado.png">
+      </div>
+    </div>
   </div>
-  <div class="seat" v-if="localSeat==='1'">
-    <img class="seat" src="../../assets/Ocupado.png" >
-  </div>
-
 </template>
 
-
 <style scoped>
+.seat-wrapper {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  max-width: 100%;
+}
+
+.seat-container {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.number {
+  z-index: 2;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  pointer-events: none;
+}
+
 .seat {
   display: flex;
-  width: 100%;
-  max-height: 56px;
+  justify-content: center;
+  align-items: center;
   max-width: 56px;
+  max-height: 56px;
+}
+
+.seat-image {
+  z-index: 1;
+  max-width: 56px;
+  max-height: 56px;
 }
 </style>
+
+
+
+
+
 
